d38a6911590d461a66e92fa654103d351ddb98b6
xyq <xyq@localhost.localdomain>
2015-05-06 17:47:29 +0800

    modify router.go

--- routers/router.go
+++ routers/router.go
@@ -7,4 +7,6 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+    beego.Router("/xyq/admin/login",&controllers.AdminLoginController{})
+    beego.Router("/xyq/admin/main", &controllers.AdminMainController{})
 }
